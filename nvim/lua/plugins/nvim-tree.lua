5a9e49132649a7b4ee37ddd498ae14d518d0b960
ajay <ajaykarunakaran987@gmail.com>
2025-05-29 10:18:31 +0530

    my nvim config

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,16 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+        vim.keymap.set('n', '<leader>e', "<cmd>NvimTreeToggle<CR>", {desc = "Toggle [E]xplorer"})
+        require("nvim-tree").setup({
+            view = {
+                width = 30, 
+                side = "left",
+                number = true, 
+                relativenumber = true,
+            }, 
+            hijack_netrw = true,
+            auto_reload_on_write = true,
+        })
+    end
+}

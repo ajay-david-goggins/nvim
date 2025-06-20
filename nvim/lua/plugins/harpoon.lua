faffb2e9b62a880235612c7611e96b97bb365f38
Ajay <ajaykarunakaran987@gmail.com>
2025-06-20 12:23:22 +0530

    from my nvim update

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,13 +1,34 @@
 return {
-    "ThePrimeagen/harpoon",
-    event = "VeryLazy",
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
-    config = function()
-        -- Set a vim motion to <Shift>m to mark a file with harpoon
-        vim.keymap.set("n", "<s-m>", "<cmd>lua require('harpoon.mark').add_file()<cr>", {desc = "Harpoon Mark File"})
-        -- Set a vim motion to the tab key to open the harpoon menu to easily navigate frequented files
-        vim.keymap.set("n", "<TAB>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", {desc = "Harpoon Toggle Menu"})
+  "ThePrimeagen/harpoon",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
+  config = function()
+    local mark = require("harpoon.mark")
+    local ui = require("harpoon.ui")
+
+    -- 🔖 Mark file with <Shift>m
+    vim.keymap.set("n", "<S-m>", function()
+      mark.add_file()
+    end, { desc = "Harpoon Mark File" })
+
+    -- 📁 Toggle Harpoon menu with <Tab>
+    vim.keymap.set("n", "<Tab>", function()
+      ui.toggle_quick_menu()
+    end, { desc = "Harpoon Toggle Menu" })
+
+    -- 🔢 Jump to files 1–9 using <Alt+1> to <Alt+9>
+    for i = 1, 9 do
+      vim.keymap.set("n", "<A-" .. i .. ">", function()
+        ui.nav_file(i)
+      end, { desc = "Harpoon: Go to file " .. i })
     end
+
+    -- 🔟 Jump to file 10 using <Alt+0>
+    vim.keymap.set("n", "<A-0>", function()
+      ui.nav_file(10)
+    end, { desc = "Harpoon: Go to file 10" })
+  end,
 }
+

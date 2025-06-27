825ad92af1b74b6d99adc2034a747074042519a6
Ajay <ajaykarunakaran987@gmail.com>
2025-06-27 12:12:26 +0530

    Angular update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,62 +1,69 @@
 return {
+    -- Mason Installer
     {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
+
+    -- Mason LSP Installer
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "lua_ls",        -- Lua
-                    "ts_ls",         -- JavaScript/TypeScript (your original)
+                    "ts_ls",      -- JavaScript/TypeScript
                     "jdtls",         -- Java
                     "html",          -- HTML
                     "cssls",         -- CSS
                     "clangd",        -- C/C++
-                    "emmet_ls"       -- ✅ Added Emmet LSP
+                    "emmet_ls"       -- Emmet
+                    -- ✨ angularls is NOT managed here
                 },
             })
-        end
+        end,
     },
+
+    -- DAP Support
     {
         "jay-babu/mason-nvim-dap.nvim",
         config = function()
             require("mason-nvim-dap").setup({
                 ensure_installed = { "java-debug-adapter", "java-test" }
             })
-        end
+        end,
     },
+
+    -- Java LSP (JDTLS)
     {
         "mfussenegger/nvim-jdtls",
         dependencies = {
             "mfussenegger/nvim-dap",
-        }
+        },
     },
+
+    -- LSP Setup
     {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+            -- Standard LSPs
             lspconfig.lua_ls.setup({ capabilities = capabilities })
-            lspconfig.ts_ls.setup({ capabilities = capabilities })  -- ✅ using ts_ls as you did
+            lspconfig.ts_ls.setup({ capabilities = capabilities }) -- ts_ls (not ts_ls)
             lspconfig.html.setup({ capabilities = capabilities })
             lspconfig.cssls.setup({ capabilities = capabilities })
             lspconfig.clangd.setup({ capabilities = capabilities })
 
-            -- ✅ Emmet LSP setup (just added)
+            -- Emmet LSP
             lspconfig.emmet_ls.setup({
                 capabilities = capabilities,
                 filetypes = {
-                    "html",
-                    "css",
-                    "php",
-                    "blade",
-                    "javascriptreact",
-                    "typescriptreact"
+                    "html", "css", "php", "blade",
+                    "javascriptreact", "typescriptreact"
                 },
                 init_options = {
                     html = {
@@ -67,15 +74,34 @@ return {
                 }
             })
 
-            -- LSP Keymaps
-            vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
-            vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
-            vim.keymap.set("n", "<leader>cr", require("telescope.builtin").lsp_references, { desc = "[C]ode Goto [R]eferences" })
-            vim.keymap.set("n", "<leader>ci", require("telescope.builtin").lsp_implementations, { desc = "[C]ode Goto [I]mplementations" })
+            lspconfig.angularls.setup({
+                capabilities = capabilities,
+                cmd = {
+                    "ngserver",
+                    "--stdio",
+                    "--tsProbeLocations", "/usr/lib/node_modules",
+                    "--ngProbeLocations", "/usr/lib/node_modules"
+                },
+                on_new_config = function(new_config, new_root_dir)
+                    new_config.cmd = {
+                        "ngserver",
+                        "--stdio",
+                        "--tsProbeLocations", "/usr/lib/node_modules",
+                        "--ngProbeLocations", "/usr/lib/node_modules"
+                    }
+                end,
+                root_dir = require("lspconfig").util.root_pattern("angular.json", "project.json", ".git"),
+                filetypes = { "typescript", "html" },
+            })
+            -- 🔑 Keybindings
+            vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over" })
+            vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode [D]efinition" })
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
+            vim.keymap.set("n", "<leader>cr", require("telescope.builtin").lsp_references, { desc = "[C]ode [R]eferences" })
+            vim.keymap.set("n", "<leader>ci", require("telescope.builtin").lsp_implementations, { desc = "[C]ode [I]mplementations" })
             vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
-            vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
-        end
-    }
+            vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode [D]eclaration" })
+        end,
+    },
 }
 

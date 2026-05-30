fa082c781f2cb1d935e3335833f0c636b3731ab9
Ajay Karunakaran <Ajay.Karunakaran@digitalhoop.io>
2026-05-30 09:50:58 +0530

    update : harpoon is updated

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,5 +1,6 @@
 return {
   "ThePrimeagen/harpoon",
+  branch="harpoon2",
   event = "VeryLazy",
   dependencies = {
     "nvim-lua/plenary.nvim"
@@ -13,7 +14,7 @@ return {
       mark.add_file()
     end, { desc = "Harpoon Mark File" })
 
-    -- 📁 Toggle Harpoon menu with <Tab>
+    -- 📁 Toggle Harpoon menu with <leader>th
     vim.keymap.set("n", "<leader>th", function()
       ui.toggle_quick_menu()
     end, { desc = "Harpoon Toggle Menu" })

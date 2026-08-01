45d535a29c417bf9936c133fb6d05f14b53d41f5
Ajay Karunakaran <Ajay.Karunakaran@digitalhoop.io>
2026-08-01 12:07:38 +0530

    update : tab and spacing changed 4 to 2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,34 +1,52 @@
 return {
   "ThePrimeagen/harpoon",
-  branch="harpoon2",
+  branch = "harpoon2",
   event = "VeryLazy",
   dependencies = {
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
   },
-  config = function()
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
 
-    -- 🔖 Mark file with <Shift>m
+  config = function()
+    local harpoon = require("harpoon")
+
+    -- REQUIRED
+    harpoon:setup({
+      settings = {
+        save_on_toggle = true,
+        sync_on_ui_close = true,
+      },
+    })
+
+    -- 🔖 Mark current file
     vim.keymap.set("n", "<S-m>", function()
-      mark.add_file()
+      harpoon:list():add()
     end, { desc = "Harpoon Mark File" })
 
-    -- 📁 Toggle Harpoon menu with <leader>th
+    -- 📁 Toggle Harpoon menu
     vim.keymap.set("n", "<leader>th", function()
-      ui.toggle_quick_menu()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = "Harpoon Toggle Menu" })
 
-    -- 🔢 Jump to files 1–9 using <Alt+1> to <Alt+9>
+    -- 🔢 Jump to files 1-9
     for i = 1, 9 do
       vim.keymap.set("n", "<A-" .. i .. ">", function()
-        ui.nav_file(i)
-      end, { desc = "Harpoon: Go to file " .. i })
+        harpoon:list():select(i)
+      end, { desc = "Harpoon File " .. i })
     end
 
-    -- 🔟 Jump to file 10 using <Alt+0>
+    -- 🔟 Jump to file 10
     vim.keymap.set("n", "<A-0>", function()
-      ui.nav_file(10)
-    end, { desc = "Harpoon: Go to file 10" })
+      harpoon:list():select(10)
+    end, { desc = "Harpoon File 10" })
+
+    -- ⏭ Next file
+    vim.keymap.set("n", "<leader>hn", function()
+      harpoon:list():next()
+    end, { desc = "Harpoon Next" })
+
+    -- ⏮ Previous file
+    vim.keymap.set("n", "<leader>hp", function()
+      harpoon:list():prev()
+    end, { desc = "Harpoon Previous" })
   end,
 }
